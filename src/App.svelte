1d53e759c178cf55144052b3cb42cfcac8e6c966
Vecopotryx <repsejk@gmail.com>
2022-04-09 11:47:59 +0200

    Added unzipit for extracting epub contents

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	export let name: string;
+	import { unzip } from "unzipit";
+
+
+	async function readFiles(e: Event) {
+		let file = (e.target as HTMLInputElement).files[0];
+
+		const { entries } = await unzip(file);
+		console.log(entries);
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<input type="file" on:change={(e) => readFiles(e)}/>
 </main>
 
 <style>
