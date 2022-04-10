e1f6b95c4c1ec0ddc621b512d5306869a9f47146
Vecopotryx <repsejk@gmail.com>
2022-04-10 18:45:20 +0200

    Add ability to step between book sections

--- src/Reader.svelte
+++ src/Reader.svelte
@@ -2,22 +2,47 @@
     import type { Book } from "./parse";
 
     export let book: Book;
+
+    let section = 0;
+
+    $: percent = Math.floor(100*(section / book.contents.length));
 </script>
 
 <main>
     <div id="topbar">
+        <p id="percentage">{percent}%</p>
+        <button on:click={() => section -= 2 }>Previous</button>
+
         <h4>
             <b>{book !== undefined ? book.meta.title + " - " : ""}</b>
             {book !== undefined ? book.meta.author : ""}
         </h4>
+
+        <button on:click={() => section += 2 }>Next</button>
     </div>
 
-    {#each book !== undefined ? book.contents : "" as content}
-        {@html content}
-    {/each}
+    <div id="container">
+        {@html book.contents[section]}
+        {@html book.contents[section+1]}
+
+
+        
+    </div>
 </main>
 
 <style>
+    #percentage {
+        float: left;
+        margin: auto;
+        display: inline;
+        color: black;
+    }
+
+    #container {
+        height: 100%;
+        columns: 2;
+    }
+
     #topbar {
         text-align: center;
         height: 2em;
@@ -28,6 +53,6 @@
 
     h4 {
         display: inline;
-        color: black; 
+        color: black;
     }
 </style>
