aefdf86a918fb744d200a2e400f931b80227d041
Vecopotryx <repsejk@gmail.com>
2022-04-11 19:16:39 +0200

    Add ability to drop file on page to load it

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,47 @@
 
 	let book: Book;
 
-	async function readFiles(e: Event) {
-		let file = (e.target as HTMLInputElement).files[0];
-
+	async function readFiles(file: object) {
 		book = await parser(file);
 		reading = true;
 	}
 
 	let reading = false;
+
+	let dragging = false;
+
+	window.addEventListener("dragenter", (e) => {
+		if (e.dataTransfer.types.includes("Files")) {
+			dragging = true;
+		}
+	});
+
+	window.addEventListener("dragleave", (e) => {
+		dragging = false;
+		e.preventDefault();
+	});
+
+	window.addEventListener("dragover", (e) => e.preventDefault());
+
+	window.addEventListener("drop", (e) => {
+		e.preventDefault();
+		dragging = false;
+		readFiles(e.dataTransfer.files[0]);
+	});
+
+	const useFileInput = (e: Event) => {
+		readFiles((e.target as HTMLInputElement).files[0]);
+	};
 </script>
 
 <main>
+	{#if dragging}
+		<h2>Drop file</h2>
+	{/if}
 	{#if reading}
 		<Reader {book} />
 	{:else}
-		<input type="file" on:change={(e) => readFiles(e)} />
+		<input type="file" on:change={(e) => useFileInput(e)} />
 	{/if}
 </main>
 
