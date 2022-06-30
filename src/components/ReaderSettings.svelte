5b1f60269165cfc875649615f6c572382cb77acb
Jesper <repsejk@gmail.com>
2022-06-30 14:35:29 +0200

    Create generic component for theme picker

--- src/components/ReaderSettings.svelte
+++ src/components/ReaderSettings.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import Popover from "./Popover.svelte";
+    import ThemePicker from "./ThemePicker.svelte";
+    
     export let theme: string;
 
     export let settings = {
@@ -37,40 +39,10 @@
             bind:value={settings.fontSize}
         />
 
-        <div id="themePicker">
-            <div
-                style="background-color: #f6e4bd; color: #000"
-                on:click={() => (theme = "warm")}
-            >
-                warm
-            </div>
-            <div
-                style="background-color: #000; color: #e9e9e9"
-                on:click={() => (theme = "black")}
-            >
-                black
-            </div>
-            <div
-                style="background-color: #032038; color: #e9e9e9"
-                on:click={() => (theme = "dark")}
-            >
-                dark
-            </div>
-            <div
-                style="background-color: #fff; color: #000"
-                on:click={() => (theme = "light")}
-            >
-                light
-            </div>
-        </div>
+        <ThemePicker bind:theme/>
     </Popover>
 </main>
 
 <style>
-    #themePicker > div {
-        border-radius: 0.2em;
-        margin: 0.2em;
-        cursor: pointer;
-        user-select: none;
-    }
+
 </style>
---
+++ src/components/ThemePicker.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let theme: string;
+</script>
+
+<main id="themePicker">
+    <div
+        style="background-color: #f6e4bd; color: #000"
+        on:click={() => (theme = "warm")}
+    >
+        warm
+    </div>
+    <div
+        style="background-color: #000; color: #e9e9e9"
+        on:click={() => (theme = "black")}
+    >
+        black
+    </div>
+    <div
+        style="background-color: #032038; color: #e9e9e9"
+        on:click={() => (theme = "dark")}
+    >
+        dark
+    </div>
+    <div
+        style="background-color: #fff; color: #000"
+        on:click={() => (theme = "light")}
+    >
+        light
+    </div>
+</main>
+
+<style>
+    #themePicker > div {
+        border-radius: 0.2em;
+        margin: 0.2em;
+        cursor: pointer;
+        user-select: none;
+    }
+</style>
