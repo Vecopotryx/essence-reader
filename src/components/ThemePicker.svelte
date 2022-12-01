aac923e9d26a90021e013fb89ea018c50270711a
Jesper <Repsejk@gmail.com>
2022-12-01 19:39:34 +0100

    Use buttons instead of divs in ThemePicker

--- src/components/ThemePicker.svelte
+++ src/components/ThemePicker.svelte
@@ -5,35 +5,40 @@
     };
 </script>
 
-<div
+<button
     style="background-color: #f6e4bd; color: #000"
     on:click={() => setTheme("warm")}
 >
     warm
-</div>
-<div
+</button>
+<button
     style="background-color: #000; color: #e9e9e9"
     on:click={() => setTheme("black")}
 >
     black
-</div>
-<div
+</button>
+<button
     style="background-color: #032038; color: #e9e9e9"
     on:click={() => setTheme("dark")}
 >
     dark
-</div>
-<div
+</button>
+<button
     style="background-color: #fff; color: #000"
     on:click={() => setTheme("light")}
 >
     light
-</div>
+</button>
 
 <style>
-    div {
+    button {
+        display: block;
+        width: 90%;
+        border: none;
+        font-family: inherit;
+        font-size: inherit;
         border-radius: 0.2em;
-        margin: 0.2em;
+        margin: 0.4em auto;
         cursor: pointer;
         user-select: none;
     }
