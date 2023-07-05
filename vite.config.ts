5e5805c5b528fa688fc2fa1a8ddb0d2fdac3b041
Jesper <Repsejk@gmail.com>
2023-07-05 17:31:19 +0200

    Start migration to SvelteKit using template

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/essence-reader/',
-  plugins: [svelte()]
-})
+	plugins: [sveltekit()]
+});
