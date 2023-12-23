dd4aa412c5db9845cc2594bdd0ea8cf7e5069efc
Jesper <contact@vecopotryx.com>
2023-12-23 20:12:31 +0100

    Refactor parsing logic

--- src/lib/services/parse.ts
+++ src/lib/services/parse.ts
@@ -4,65 +4,60 @@ import { relativeToAbs, removeHash } from '$lib/utils';
 const domParser = new DOMParser();
 
 const parseOpf = (opf: string, opfPath: string) => {
-	const opfDocument = domParser.parseFromString(opf, 'text/xml');
+	const opfDocument = domParser.parseFromString(opf, 'application/xml');
 
-	const metadata = opfDocument.querySelector('metadata');
-	if (!metadata) throw new Error('Metadata element not found in OPF');
-	const { title, author, coverId } = parseMeta(metadata);
-
+	const metadataElement = opfDocument.querySelector('metadata');
 	const manifestElement = opfDocument.querySelector('manifest');
-	if (!manifestElement) throw new Error('Manifest element not found in OPF');
-	const manifestItems = parseManifest(manifestElement, opfPath);
-
 	const spineElement = opfDocument.querySelector('spine');
+	if (!metadataElement) throw new Error('Metadata element not found in OPF');
+	if (!manifestElement) throw new Error('Manifest element not found in OPF');
 	if (!spineElement) throw new Error('Spine element not found in OPF');
+
+	const { title, author } = parseMeta(metadataElement);
+	const coverPath = getCoverPath(manifestElement, metadataElement, opfPath);
+	const manifestItems = parseManifest(manifestElement, opfPath);
 	const spine = parseSpine(spineElement, manifestItems);
-
-	let coverPath: string | undefined = undefined;
-	if (coverId && manifestItems.has(coverId)) {
-		// Found cover following EPUB 2 spec
-		coverPath = manifestItems.get(coverId);
-	} else {
-		// Try to find cover following EPUB 3 spec
-		const coverItem = manifestElement.querySelector("item[properties='cover-image']");
-		const href = coverItem?.getAttribute('href');
-
-		if (href) {
-			coverPath = relativeToAbs(href, opfPath);
-		}
-	}
-
-	const ncxPath = manifestItems.get('ncx');
+	const ncxPath = getNcxPath(manifestElement, opfPath);
 
 	return { title, author, coverPath, spine, ncxPath };
 };
 
-const parseMeta = (
-	meta: Element
-): { title: string; author: string[]; coverId: string | undefined } => {
+const getNcxPath = (manifest: Element, opfPath: string): string | undefined => {
+	const ncxItem = manifest.querySelector("item[media-type='application/x-dtbncx+xml']");
+	const href = ncxItem?.getAttribute('href');
+	if (!href) return undefined;
+	return relativeToAbs(href, opfPath);
+};
+
+const getCoverPath = (manifest: Element, meta: Element, opfPath: string): string | undefined => {
+	const coverId = meta.querySelector("[name='cover']")?.getAttribute('content');
+
+	const coverItem = manifest.querySelector(`item[id='${coverId}']`);
+	const href = coverItem?.getAttribute('href');
+	if (!href) {
+		// Try epub 3 cover image if epub 2 cover image not found
+		const coverItem = manifest.querySelector("item[properties='cover-image']");
+		const href = coverItem?.getAttribute('href');
+		if (!href) return undefined;
+		return relativeToAbs(href, opfPath);
+	}
+	return relativeToAbs(href, opfPath);
+};
+
+const parseMeta = (meta: Element): { title: string; author: string[] } => {
 	const titleElement: Element | null = meta.querySelector('title');
 	const title: string = titleElement?.textContent || '';
 
 	const authorElements: NodeListOf<Element> = meta.querySelectorAll('creator');
 	const author: string[] = Array.from(authorElements).map((v: Element) => v.textContent || '');
 
-	const coverElement: Element | null = meta.querySelector("[name='cover']");
-	const coverId: string | undefined = coverElement?.getAttribute('content') ?? undefined;
-
-	return { title, author, coverId };
+	return { title, author };
 };
 
 const parseManifest = (manifest: Element, opfPath: string): Map<string, string> => {
 	const manifestItems: Map<string, string> = new Map();
 
 	for (const item of manifest.children) {
-		if (item.getAttribute('media-type') === 'application/x-dtbncx+xml') {
-			const href = item.getAttribute('href');
-			if (href) {
-				manifestItems.set('ncx', relativeToAbs(href, opfPath));
-			}
-			continue;
-		}
 		const id = item.getAttribute('id');
 		const href = item.getAttribute('href');
 		if (id && href) {
@@ -128,6 +123,11 @@ export const parseEpub = async (epub: File): Promise<{ meta: Metadata; book: Boo
 
 		const { title, author, coverPath, spine, ncxPath } = parseOpf(opf, opfPath);
 
+		let cover: Blob | undefined;
+		if (coverPath) {
+			cover = await entries[coverPath].blob();
+		}
+
 		let toc: TableOfContentsItem[] = [];
 		if (ncxPath) {
 			// Allowing for books without a ncx file
@@ -135,22 +135,7 @@ export const parseEpub = async (epub: File): Promise<{ meta: Metadata; book: Boo
 			toc = parseToc(ncx, ncxPath, spine);
 		}
 
-		let cover: Blob | undefined;
-		try {
-			if (coverPath) {
-				cover = await entries[coverPath].blob();
-			}
-		} catch (e) {
-			cover = undefined;
-		}
-
-		const meta: Metadata = {
-			title,
-			author,
-			cover,
-			progress: 0,
-			length: spine.length - 1
-		};
+		const meta: Metadata = { title, author, cover, progress: 0, length: spine.length - 1 };
 		const book: Book = { spine, toc, file: epub };
 
 		return { meta, book };
